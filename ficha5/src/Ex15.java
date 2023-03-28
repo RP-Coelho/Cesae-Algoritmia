1f947b8817c670428f3955291e2cf8871ade0cf0
RP-Coelho <126895302+RP-Coelho@users.noreply.github.com>
2023-03-28 12:16:04 +0100

    update ex 15

--- ficha5/src/Ex15.java
+++ ficha5/src/Ex15.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.sql.SQLOutput;
 import java.util.DoubleSummaryStatistics;
 import java.util.Scanner;
 
@@ -5,7 +7,10 @@ public class Ex15 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int id, x, y;
+        int id, x = 12, i = 0;
+        double irst = 0, vbt = 0, vlt = 0, salt = 0, encargost = 0;
+        int[] y = new int[x];
+        double[] encargos = new double[x], vb = new double[x], sst = new double[x], sal = new double[x], irs = new double[x], ss = new double[x], tsu = new double[x], vl = new double[x];
         String rsp = "N", nome, cargo;
 
         do {
@@ -31,40 +36,176 @@ public class Ex15 {
                 } while (!rsp.equals("N"));
                 System.out.println("Deseja Prosseguir?");
                 rsp = input.next();
-            }while (!rsp.equals("S"));
-
+            } while (!rsp.equals("S"));
             do {
-                System.out.println("Qual o seu nome de Funcionario");
-                nome = input.next();
-                System.out.println("Deseja prosseguir?");
+                do {
+                    System.out.println("Qual o seu nome de Funcionario");
+                    nome = input.next();
+                    System.out.println("Deseja refazer?");
+                    rsp = input.next();
+                } while (!rsp.equals("N"));
+                System.out.println("Deseja Prosseguir?");
                 rsp = input.next();
-            } while (!rsp.equals("N"));
-
+            } while (!rsp.equals("S"));
             do {
-                System.out.println("Qual o seu cargo de Funcionario");
-                cargo = input.next();
-                System.out.println("Deseja prosseguir?");
+                do {
+                    System.out.println("Qual o seu cargo de Funcionario");
+                    cargo = input.next();
+                    if(cargo.equals("C")||cargo.equals("E")||cargo.equals("A")) {
+                        System.out.println("Deseja refazer?");
+                        rsp = input.next();
+                    }else{
+                        System.out.println("Cargo Invalido! Tente de novo!");
+                        rsp="S";
+                    }
+                } while (!rsp.equals("N"));
+                System.out.println("Deseja Prosseguir?");
                 rsp = input.next();
-            } while (!rsp.equals("N"));
-
+            } while (!rsp.equals("S"));
             do {
-                System.out.println("Quantos meses trabalhou");
-                x = input.nextInt();
-                System.out.println("Deseja prosseguir?");
+                do {
+                    System.out.println("Quantos meses trabalhou");
+                    x = input.nextInt();
+                    if (x < 0 && x < 13) {
+                        System.out.println("Erro repita! de novo!");
+                        rsp = "S";
+                    } else {
+                        System.out.println("Deseja refazer?");
+                        rsp = input.next();
+                    }
+                } while (!rsp.equals("N"));
+                System.out.println("Deseja Prosseguir?");
                 rsp = input.next();
-            } while (!rsp.equals("N"));
-
+            } while (!rsp.equals("S"));
             do {
-                System.out.println("Quantos dias trabalhou");
-                y = input.nextInt();
-                System.out.println("Deseja prosseguir?");
+                do {
+                    for (i = 0; i < x; i++) {
+                        System.out.println("Quantos dias trabalhou no mes " + (i+1));
+                        y[i] = input.nextInt();
+                        if (y[i] > 31 || y[i] < 0) {
+                            System.out.println("Numero dias errado");
+                            i = i - 1;
+                        }
+                    }
+                    System.out.println("Deseja refazer?");
+                    rsp = input.next();
+                } while (!rsp.equals("N"));
+                System.out.println("Deseja Prosseguir?");
                 rsp = input.next();
-            } while (!rsp.equals("N"));
+            } while (!rsp.equals("S"));
+            switch (cargo) {
+                case "E":
+                    for (i = 0; i < x; i++) {
+                        if (y[i] < 17) {
+                            vb[i] = y[i] * 40;
 
-            System.out.println("Deseja prosseguir?");
+                        } else if (y[i] < 20) {
+                            vb[i] = y[i] * 40 * 1.02;
+                        } else {
+                            vb[i] = y[i] * 40 * 1.05;
+                        }
+                        vbt = vbt + vb[i];
+                        sal[i] = y[i] * 5;
+                        salt = sal[i] + salt;
+                        if (vb[i] < 1000) {
+                            irs[i] = vb[i] * 0.1;
+                        } else if (vb[i] < 2500) {
+                            irs[i] = vb[i] * 0.2;
+                        } else {
+                            irs[i] = vb[i] * 0.3;
+                        }
+                        irst = irst + irs[i];
+                        ss[i] = vb[i] * 0.11;
+                        tsu[i] = vb[i] * 0.2375;
+
+                        sst[i] = ss[i] + tsu[i];
+                        vl[i] = vb[i] - ss[i] - irs[i];
+                        vlt = vl[i] + vlt;
+                        encargos[i] = vb[i] + sal[i] + sst[i];
+                        encargost = encargost + encargos[i];
+                    }
+
+
+                    break;
+                case "C":
+                    for (i = 0; i < x; i++) {
+                        if (y[i] < 17) {
+                            vb[i] = y[i] * 60;
+
+                        } else if (y[i] < 20) {
+                            vb[i] = y[i] * 60 * 1.02;
+                        } else {
+                            vb[i] = y[i] * 60 * 1.05;
+                        }
+                        vbt = vbt + vb[i];
+                        sal[i] = y[i] * 7.5;
+                        salt = sal[i] + salt;
+                        if (vb[i] < 1000) {
+                            irs[i] = vb[i] * 0.1;
+                        } else if (vb[i] < 2500) {
+                            irs[i] = vb[i] * 0.2;
+                        } else {
+                            irs[i] = vb[i] * 0.3;
+                        }
+                        irst = irst + irs[i];
+                        ss[i] = vb[i] * 0.11;
+                        tsu[i] = vb[i] * 0.2375;
+
+                        sst[i] = ss[i] + tsu[i];
+                        vl[i] = vb[i] - ss[i] - irs[i];
+                        vlt = vl[i] + vlt;
+                        encargos[i] = vb[i] + sal[i] + sst[i];
+                        encargost = encargost + encargos[i];
+                    }
+                    break;
+
+                case "A":
+                    for (i = 0; i < x; i++) {
+                        if (y[i] < 17) {
+                            vb[i] = y[i] * 80;
+
+                        } else if (y[i] < 20) {
+                            vb[i] = y[i] * 80 * 1.02;
+                        } else {
+                            vb[i] = y[i] * 80 * 1.05;
+                        }
+                        vbt = vbt + vb[i];
+                        sal[i] = y[i] * 7.5;
+                        salt = sal[i] + salt;
+                        if (vb[i] < 1000) {
+                            irs[i] = vb[i] * 0.1;
+                        } else if (vb[i] < 2500) {
+                            irs[i] = vb[i] * 0.2;
+                        } else {
+                            irs[i] = vb[i] * 0.3;
+                        }
+                        irst = irst + irs[i];
+                        ss[i] = vb[i] * 0.09;
+                        tsu[i] = vb[i] * 0.21;
+
+                        sst[i] = ss[i] + tsu[i];
+                        vl[i] = vb[i] - ss[i] - irs[i];
+                        vlt = vl[i] + vlt;
+                        encargos[i] = vb[i] + sal[i] + sst[i];
+                        encargost = encargost + encargos[i];
+                    }
+                    break;
+
+            }
+            for (i = 0; i < x; i++) {
+                System.out.println("O valor liquido mensal dos funcionarios foi no mes " + i + "de " + vl[i]+"€");
+                System.out.println("O valor pago total mensal dos funcionarios foi no mes " + i + "de " + sst[i]+"€");
+            }
+            System.out.println("");
+            System.out.println("");
+            System.out.println("O valor liquido total do funcionario foi  " + vlt+"€");
+            System.out.println("O valor liquido total do funcionario foi  " + encargost+"€");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Deseja calcula outra função?");
             rsp = input.next();
-        } while (!rsp.equals("N"));
-
+        } while (!rsp.equals("S"));
 
     }
 }
+
